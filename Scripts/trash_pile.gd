60e108efe9e38470bcc21be3633b3e088181fcaa
Filip Doležal <fidokadan@gmail.com>
2025-01-24 11:03:47 +0100

    Added Radiators dumpster

--- Scripts/trash_pile.gd
+++ Scripts/trash_pile.gd
@@ -7,7 +7,12 @@ enum TrashTypes {METAL, GLASS, INTERIOR, MOTOR, BATTERY}
 @export var trash_type: TrashTypes
 
 func _ready() -> void:
+	self.set_collision_layer_value(2, true)
+	self.set_collision_layer_value(1, false)
+	area.set_collision_layer_value(2, true)
+	area.set_collision_layer_value(1, false)
 	area.connect("body_entered", trash_entered)
 
 func trash_entered(trash: Node3D):
-	print("yippeee")
+	if trash is CarPart:
+		trash.queue_free()
